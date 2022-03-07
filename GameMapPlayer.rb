db143c8ae72ee390a92bc3cd106eef9f0251eced
Fulgurance <zohran.londais@gmail.com>
2022-03-07 15:54:24 +0100

    Added running gesture

--- GameMapPlayer.rb
+++ GameMapPlayer.rb
@@ -1,12 +1,16 @@
 class GameMapPlayer < GameMapMovableObject
 
+    attr_accessor :animationRunningSpeed
+
     def initialize(spriteName,x,y,z,sizeX=32,sizeY=0,sizeZ=64,direction=3)
         super(spriteName,x,y,z,sizeX,sizeY,sizeZ,direction=3)
         @spriteName = spriteName
         @sprite = Sprite.new("Graphics/Player/#{@spriteName}Down.png",retro: true)
         @direction = direction
         @speed = 1.3
-        @animationSpeed = 0.20
+        @runningSpeed = 2.3
+        @animationSpeed = 0.2
+        @animationRunningSpeed = 0.15
         @currentMovementDistance = 0
         @pressedButtonTime = 0.0
         @movingFrame = 0
@@ -39,6 +43,90 @@ class GameMapPlayer < GameMapMovableObject
         super
         $gameWindow.currentGameScene.cameraY -= @speed
         $gameWindow.currentGameScene.cameraReferenceY -= @speed
+    end
+
+    def runLeft
+        @movingLeft = true
+        @currentMovementDistance += @runningSpeed
+        @realX -= @runningSpeed
+        playRunningLeft
+        $gameWindow.currentGameScene.cameraX -= @runningSpeed
+        $gameWindow.currentGameScene.cameraReferenceX -= @runningSpeed
+    end
+
+    def runRight
+        @movingRight = true
+        @currentMovementDistance += @runningSpeed
+        @realX += @runningSpeed
+        playRunningRight
+        $gameWindow.currentGameScene.cameraX += @runningSpeed
+        $gameWindow.currentGameScene.cameraReferenceX += @runningSpeed
+    end
+
+    def runUp
+        @movingUp = true
+        @currentMovementDistance += @runningSpeed
+        @realY += @runningSpeed
+        playRunningUp
+        $gameWindow.currentGameScene.cameraY += @runningSpeed
+        $gameWindow.currentGameScene.cameraReferenceY += @runningSpeed
+    end
+
+    def runDown
+        @movingDown = true
+        @currentMovementDistance += @runningSpeed
+        @realY -= @runningSpeed
+        playRunningDown
+        $gameWindow.currentGameScene.cameraY -= @runningSpeed
+        $gameWindow.currentGameScene.cameraReferenceY -= @runningSpeed
+    end
+
+    def playRunningLeft
+        if @movingFrame == 0
+            @sprite.insert("Graphics/Player/#{@spriteName}RunningLeft1.png",0,0)
+        elsif @movingFrame == 1
+            @sprite.insert("Graphics/Player/#{@spriteName}RunningLeft.png",0,0)
+        elsif @movingFrame == 2
+            @sprite.insert("Graphics/Player/#{@spriteName}RunningLeft2.png",0,0)
+        else
+            @sprite.insert("Graphics/Player/#{@spriteName}RunningLeft.png",0,0)
+        end
+    end
+
+    def playRunningRight
+        if @movingFrame == 0
+            @sprite.insert("Graphics/Player/#{@spriteName}RunningRight1.png",0,0)
+        elsif @movingFrame == 1
+            @sprite.insert("Graphics/Player/#{@spriteName}RunningRight.png",0,0)
+        elsif @movingFrame == 2
+            @sprite.insert("Graphics/Player/#{@spriteName}RunningRight2.png",0,0)
+        else
+            @sprite.insert("Graphics/Player/#{@spriteName}RunningRight.png",0,0)
+        end
+    end
+
+    def playRunningUp
+        if @movingFrame == 0
+            @sprite.insert("Graphics/Player/#{@spriteName}RunningUp1.png",0,0)
+        elsif @movingFrame == 1
+            @sprite.insert("Graphics/Player/#{@spriteName}RunningUp.png",0,0)
+        elsif @movingFrame == 2
+            @sprite.insert("Graphics/Player/#{@spriteName}RunningUp2.png",0,0)
+        else
+            @sprite.insert("Graphics/Player/#{@spriteName}RunningUp.png",0,0)
+        end
+    end
+
+    def playRunningDown
+        if @movingFrame == 0
+            @sprite.insert("Graphics/Player/#{@spriteName}RunningDown1.png",0,0)
+        elsif @movingFrame == 1
+            @sprite.insert("Graphics/Player/#{@spriteName}RunningDown.png",0,0)
+        elsif @movingFrame == 2
+            @sprite.insert("Graphics/Player/#{@spriteName}RunningDown2.png",0,0)
+        else
+            @sprite.insert("Graphics/Player/#{@spriteName}RunningDown.png",0,0)
+        end
     end
 
 end
--- GameMapScene.rb
+++ GameMapScene.rb
@@ -29,11 +29,11 @@ class GameMapScene < GameScene3D
     @objects = []
     @events = []
     @player = GameMapPlayer.new("Red",playerX,playerY,playerZ)
-    @cameraX = 0.0 + @player.realX - @player.sprite.width/2
+    @cameraX = @player.realX+@squareSize/2
     @cameraY = -320.0
     @cameraZ = 250.0
-    @cameraReferenceX = @player.realX - @player.sprite.width/2
-    @cameraReferenceY = @player.realY
+    @cameraReferenceX = @player.realX+@squareSize/2
+    @cameraReferenceY = @player.realY+@squareSize
     @cameraReferenceZ = @player.realZ
     @upX = 0.0
     @upY = 0.0
@@ -72,27 +72,59 @@ class GameMapScene < GameScene3D
   end
 
   def updateInputs
-    if (Time.now.to_f-@movementAnimationTime.to_f) > @player.animationSpeed
-      @player.movingFrame += 1
-      if @player.movingFrame == 2
-        @playBumpSound = true
+    if Gosu.button_down?(Gosu::KB_RIGHT_SHIFT)
+      if (Time.now.to_f-@movementAnimationTime.to_f) > @player.animationRunningSpeed
+        @player.movingFrame += 1
+        if @player.movingFrame == 2
+          @playBumpSound = true
+        end
+        if @player.movingFrame > 3
+          @player.movingFrame = 0
+          @playBumpSound = true
+        end
+        @movementAnimationTime = Time.now
+      else
+        @playBumpSound = false
       end
-      if @player.movingFrame > 3
-        @player.movingFrame = 0
-        @playBumpSound = true
-      end
-      @movementAnimationTime = Time.now
     else
-      @playBumpSound = false
+      if (Time.now.to_f-@movementAnimationTime.to_f) > @player.animationSpeed
+        @player.movingFrame += 1
+        if @player.movingFrame == 2
+          @playBumpSound = true
+        end
+        if @player.movingFrame > 3
+          @player.movingFrame = 0
+          @playBumpSound = true
+        end
+        @movementAnimationTime = Time.now
+      else
+        @playBumpSound = false
+      end
     end
     if @player.movingLeft && @player.currentMovementDistance < @squareSize
-      @player.moveLeft
+      if Gosu.button_down?(Gosu::KB_RIGHT_SHIFT)
+        @player.runLeft
+      else
+        @player.moveLeft
+      end
     elsif @player.movingRight && @player.currentMovementDistance < @squareSize
-      @player.moveRight
+      if Gosu.button_down?(Gosu::KB_RIGHT_SHIFT)
+        @player.runRight
+      else
+        @player.moveRight
+      end
     elsif @player.movingUp && @player.currentMovementDistance < @squareSize
-      @player.moveUp
+      if Gosu.button_down?(Gosu::KB_RIGHT_SHIFT)
+        @player.runUp
+      else
+        @player.moveUp
+      end
     elsif @player.movingDown && @player.currentMovementDistance < @squareSize
-      @player.moveDown
+      if Gosu.button_down?(Gosu::KB_RIGHT_SHIFT)
+        @player.runDown
+      else
+        @player.moveDown
+      end
     else
       @player.currentMovementDistance = 0
       @player.movingLeft = false
@@ -104,10 +136,18 @@ class GameMapScene < GameScene3D
         if (Time.now.to_f-@inputLeftTime.to_f) > @player.delayBeforeMoving
           if Gosu.button_down?(Gosu::KB_LEFT)
             if (@player.x-@squareSize) >= @mapX && directionPassable?(0)
-              @player.moveLeft
+              if Gosu.button_down?(Gosu::KB_RIGHT_SHIFT)
+                @player.runLeft
+              else
+                @player.moveLeft
+              end
               @player.x -= @squareSize
             else
-              @player.playMovingLeft
+              if Gosu.button_down?(Gosu::KB_RIGHT_SHIFT)
+                @player.playRunningLeft
+              else
+                @player.playMovingLeft
+              end
               if @playBumpSound
                 @bumpSound.play
               end
@@ -119,10 +159,18 @@ class GameMapScene < GameScene3D
         if (Time.now.to_f-@inputRightTime.to_f) > @player.delayBeforeMoving
           if Gosu.button_down?(Gosu::KB_RIGHT)
             if (@player.x+@squareSize) < (@mapX+@mapWidth) && directionPassable?(1)
-              @player.moveRight
+              if Gosu.button_down?(Gosu::KB_RIGHT_SHIFT)
+                @player.runRight
+              else
+                @player.moveRight
+              end
               @player.x += @squareSize
             else
-              @player.playMovingRight
+              if Gosu.button_down?(Gosu::KB_RIGHT_SHIFT)
+                @player.playRunningRight
+              else
+                @player.playMovingRight
+              end
               if @playBumpSound
                 @bumpSound.play
               end
@@ -134,10 +182,18 @@ class GameMapScene < GameScene3D
         if (Time.now.to_f-@inputUpTime.to_f) > @player.delayBeforeMoving
           if Gosu.button_down?(Gosu::KB_UP)
             if (@player.y+@squareSize) < (@mapY+@mapHeight) && directionPassable?(2)
-              @player.moveUp
+              if Gosu.button_down?(Gosu::KB_RIGHT_SHIFT)
+                @player.runUp
+              else
+                @player.moveUp
+              end
               @player.y += @squareSize
             else
-              @player.playMovingUp
+              if Gosu.button_down?(Gosu::KB_RIGHT_SHIFT)
+                @player.playRunningUp
+              else
+                @player.playMovingUp
+              end
               if @playBumpSound
                 @bumpSound.play
               end
@@ -149,10 +205,18 @@ class GameMapScene < GameScene3D
         if (Time.now.to_f-@inputDownTime.to_f) > @player.delayBeforeMoving
           if Gosu.button_down?(Gosu::KB_DOWN)
             if (@player.y-@squareSize) >= @mapY && directionPassable?(3)
-              @player.moveDown
+              if Gosu.button_down?(Gosu::KB_RIGHT_SHIFT)
+                @player.runDown
+              else
+                @player.moveDown
+              end
               @player.y -= @squareSize
             else
-              @player.playMovingDown
+              if Gosu.button_down?(Gosu::KB_RIGHT_SHIFT)
+                @player.playRunningDown
+              else
+                @player.playMovingDown
+              end
               if @playBumpSound
                 @bumpSound.play
               end
